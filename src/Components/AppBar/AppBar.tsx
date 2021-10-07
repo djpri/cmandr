a342c348728a65d510e57892cb0d31c1a92744f8
djpri <22898729+djpri@users.noreply.github.com>
2021-10-07 23:23:48 +0100

    moved accordion to separate file

--- src/Components/AppBar/AppBar.tsx
+++ src/Components/AppBar/AppBar.tsx
@@ -2,8 +2,10 @@
 import {
   useColorModeValue,
   Box,
+  Button,
   Stack,
   Text,
+  Link,
   StackItem,
   StackDivider,
   Heading,
@@ -14,69 +16,47 @@ import {
   AccordionIcon,
 } from "@chakra-ui/react";
 import * as React from "react";
-
+import { useSelector } from "react-redux";
+import { selectUserEmail } from "../../Redux/auth/authSlice";
 import LoginDrawer from "../LoginDrawer/LoginDrawer";
+import { ColorModeSwitcher } from "../ColorModeSwitcher/ColorModeSwitcher";
+import AppBarAccordion from "./AppBarAccordion/AppBarAccordion";
 
 function AppBar() {
+  const userEmail = useSelector(selectUserEmail);
+
   const gradient = useColorModeValue(
-    "linear(to-b, gray.100 0%, gray.200 10%, gray.50 90%, gray.50 100%)",
-    "linear(to-b, blue.800 0%, blue.700 10%, blue.800 90%, blue.800 100%)"
+    "linear(to-b, gray.100 0%, gray.200 10%, gray.50 70%, gray.50 100%)",
+    "linear(to-b, blue.800 0%, blue.700 10%, blue.800 40%, blue.800 100%)"
   );
 
   return (
     <Box
-      p="0"
+      p="2"
       h="100vh"
       position="fixed"
       bgGradient={gradient}
       w="250px"
-      borderRight="1px"
+      top="0"
+      // borderRight="2px"
       borderColor="gray.500"
     >
-      <Box p="5">
-        <LoginDrawer />
+      <Box p="5" display="flex" flexDirection="column">
+        {userEmail !== null ? (
+          <>
+            <Button bgColor="purple.500" color="whiteAlpha.900">
+              {userEmail}
+            </Button>
+            <LoginDrawer />
+          </>
+        ) : (
+          <LoginDrawer />
+        )}
+        <ColorModeSwitcher justifySelf="flex-end" />
       </Box>
       <Stack mt="10">
-        <Heading as="h3" pl="5">
-          Menu
-        </Heading>
-        <Text p="5">Categories</Text>
         <StackItem>
-          <Accordion defaultIndex={[0]} allowMultiple w="100%">
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                    Section 1 title
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </AccordionPanel>
-            </AccordionItem>
-
-            <AccordionItem>
-              <h2>
-                <AccordionButton>
-                  <Box flex="1" textAlign="left">
-                    Section 2 title
-                  </Box>
-                  <AccordionIcon />
-                </AccordionButton>
-              </h2>
-              <AccordionPanel pb={4}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat.
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+          <AppBarAccordion />
         </StackItem>
       </Stack>
     </Box>
