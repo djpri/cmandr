1a20e403f40e1c3913749beba742efe203488631
djpri <djpritchard70@live.com>
2021-10-07 23:23:48 +0100

    moved accordion to separate file

---
+++ src/Components/AppBar/AppBarAccordion/AppBarAccordion.tsx
@@ -0,0 +1,66 @@
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionIcon,
+  AccordionPanel,
+  Box,
+  Link,
+  Stack,
+  Text,
+} from "@chakra-ui/react";
+import * as React from "react";
+import { Link as RouterLink } from "react-router-dom";
+
+function AppBarAccordion() {
+  return (
+    <Accordion defaultIndex={[0]} allowMultiple w="100%">
+      <AccordionItem>
+        <AccordionButton>
+          <Box flex="1" textAlign="left">
+            <Text fontWeight="700" letterSpacing="1px">
+              Menu
+            </Text>
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+        <AccordionPanel pb={4}>
+          <Stack>
+            <Link as={RouterLink} to="/dashboard">
+              Dashboard
+            </Link>
+            <Link as={RouterLink} to="/manage-commands">
+              Command manager
+            </Link>
+            <Link as={RouterLink} to="/manage-commands">
+              Settings
+            </Link>
+          </Stack>
+        </AccordionPanel>
+      </AccordionItem>
+
+      <AccordionItem>
+        <AccordionButton>
+          <Box flex="1" textAlign="left">
+            <Text fontWeight="700" letterSpacing="1px">
+              Categories
+            </Text>
+          </Box>
+          <AccordionIcon />
+        </AccordionButton>
+        <AccordionPanel pb={4}>
+          <Stack>
+            <Text>All commands</Text>
+            <Text>General</Text>
+            <Text>Git</Text>
+            <Text>npm</Text>
+            <Text>yarn</Text>
+            <Text>baba</Text>
+          </Stack>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
+  );
+}
+
+export default AppBarAccordion;
