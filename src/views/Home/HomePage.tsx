16028c1f360d2ad6b817147dd572e52f65769b79
djpri <22898729+djpri@users.noreply.github.com>
2022-01-20 18:59:52 +0000

    added navigational links to dashboard page

--- src/views/Home/HomePage.tsx
+++ src/views/Home/HomePage.tsx
@@ -1,15 +1,71 @@
-import { Box, Grid, VStack, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Grid,
+  Heading,
+  GridItem,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import * as React from "react";
+import { useSelector } from "react-redux";
 import UserLayout from "../../layout/UserLayout";
+import { selectCategoriesWithIds } from "../../redux/commands/commandsSlice";
+import { selectLinkCategories } from "../../redux/links/linksSlice";
+import { Link as RouterLink } from "react-router-dom";
 
 function HomePage() {
+  const commandCategories = useSelector(selectCategoriesWithIds);
+  const linkCategories = useSelector(selectLinkCategories);
+  const linkBgColor = useColorModeValue("green.50", "teal.900");
+  const linkBgHoverColor = useColorModeValue("green.100", "teal.600");
+
   return (
     <UserLayout>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <VStack spacing={8}>
-            <Text>Commander</Text>
-          </VStack>
+      <Box fontSize="xl">
+        <Heading as="h1">Commands</Heading>
+        <Grid my="30px" gap={3} templateColumns="repeat(6, 1fr)">
+          {commandCategories.map((item) => (
+            <GridItem
+              as={RouterLink}
+              to={`/commands/${item.id}`}
+              boxShadow="base"
+              p="5px 10px"
+              rounded="md"
+              textAlign="left"
+              bgColor={linkBgColor}
+              _hover={{
+                bgColor: linkBgHoverColor,
+              }}
+              fontFamily="Lato"
+              fontSize="lg"
+              fontWeight="bold"
+              transition="0.1s"
+            >
+              {item.name}
+            </GridItem>
+          ))}
+        </Grid>
+        <Heading as="h1">Links</Heading>
+        <Grid my="30px" gap={3} templateColumns="repeat(6, 1fr)">
+          {linkCategories.map((item) => (
+            <GridItem
+              as={RouterLink}
+              to={`/commands/${item.id}`}
+              boxShadow="base"
+              p="5px 10px"
+              rounded="md"
+              textAlign="left"
+              bgColor={linkBgColor}
+              _hover={{
+                bgColor: linkBgHoverColor,
+              }}
+              fontFamily="Lato"
+              fontSize="lg"
+              fontWeight="bold"
+              transition="0.1s"
+            >
+              {item.name}
+            </GridItem>
+          ))}
         </Grid>
       </Box>
     </UserLayout>
