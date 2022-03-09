caef3e8de5aa4e63ab3655433d78fd7657a65e9a
djpri <22898729+djpri@users.noreply.github.com>
2022-03-09 15:53:27 +0000

    added authentication template

--- src/components/layout/UserLayout.tsx
+++ src/components/layout/UserLayout.tsx
@@ -1,41 +1,39 @@
-import { useIsAuthenticated } from "@azure/msal-react";
+import { AuthenticatedTemplate } from "@azure/msal-react";
 import { Container } from "@chakra-ui/layout";
 import * as React from "react";
-import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import { selectIsSidebarOpen } from "redux/layout/layoutSlice";
 import NavBar from "./NavBar/NavBar";
 import SideBar from "./SideBar/SideBar";
 
+// https://github.com/AzureAD/microsoft-authentication-library-for-js/blob/dev/lib/msal-react/docs/getting-started.md#determining-whether-a-user-is-authenticated
+
 function UserLayout({ children }) {
   const isSidebarOpen = useSelector(selectIsSidebarOpen);
   //const navigate = useNavigate();
   //const location = useLocation();
-  const isAuthenticated = useIsAuthenticated();
-
-  useEffect(() => {
-    console.log(`Is authenticated? ${isAuthenticated}`);
-  }, [isAuthenticated]);
 
   // useEffect(() => {
   //   if (!isLoggedIn) navigate("/account/login", { state: { from: location } });
   // }, [isLoggedIn, navigate, location]);
 
   return (
-    <React.Fragment>
+    <>
       <NavBar />
-      <SideBar />
-      <Container
-        maxW={isSidebarOpen ? "70%" : "97.5%"}
-        maxH="80vh"
-        mt="80px"
-        mb="50px"
-        ml={isSidebarOpen ? "300px" : "2.5%"}
-        position="relative"
-      >
-        {children}
-      </Container>
-    </React.Fragment>
+      <AuthenticatedTemplate>
+        <SideBar />
+        <Container
+          maxW={isSidebarOpen ? "70%" : "97.5%"}
+          maxH="80vh"
+          mt="80px"
+          mb="50px"
+          ml={isSidebarOpen ? "300px" : "2.5%"}
+          position="relative"
+        >
+          {children}
+        </Container>
+      </AuthenticatedTemplate>
+    </>
   );
 }
 
