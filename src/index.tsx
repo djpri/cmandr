2bb530c830ccc7256bae79af1f27eeba6f9ed2d6
djpri <22898729+djpri@users.noreply.github.com>
2023-03-17 11:52:11 +0000

    vite-migration

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import { msalConfig } from "auth/authConfig";
 import * as React from "react";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "redux/store";
@@ -25,8 +25,9 @@ const queryClient = new QueryClient({
 });
 
 const msalInstance = new PublicClientApplication(msalConfig);
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <ColorModeScript />
     <BrowserRouter>
@@ -45,7 +46,6 @@ ReactDOM.render(
       </DndProvider>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
