3f16a63e999d61ca1c75860ce55a2b8a17e354e6
djpri <djpritchard70@live.com>
2023-04-07 09:22:21 +0100

    removed lazy loaded routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,17 +2,26 @@ import { Spinner } from "@chakra-ui/react";
 import UserLayout from "components/layout/UserLayout";
 import { FC, lazy, Suspense } from "react";
 import { createBrowserRouter } from "react-router-dom";
+
+import Dashboard from "views/Dashboard";
 import Home from "views/Home";
 import Loading from "views/Loading";
+import AllCommands from "views/commands/AllCommands";
+import CommandCategory from "views/commands/CommandCategory";
+import AllLinks from "views/links/AllLinks";
+import LinkCategory from "views/links/LinkCategory";
+import AddSnippetPage from "views/snippets/AddSnippetPage";
+import AllSnippetsPage from "views/snippets/AllSnippets";
+import SnippetCategoryPage from "views/snippets/SnippetCategory";
 
-const Dashboard = lazy(() => import("./views/Dashboard"));
-const AllCommands = lazy(() => import("./views/commands/AllCommands"));
-const CommandCategory = lazy(() => import("./views/commands/CommandCategory"));
-const AllLinks = lazy(() => import("./views/links/AllLinks"));
-const LinkCategory = lazy(() => import("./views/links/LinkCategory"));
-const AllSnippetsPage = lazy(() => import("./views/snippets/AllSnippets"));
-const SnippetCategoryPage = lazy(() => import("./views/snippets/SnippetCategory"));
-const AddSnippetPage = lazy(() => import("./views/snippets/AddSnippetPage"));
+// const Dashboard = lazy(() => import("./views/Dashboard"));
+// const AllCommands = lazy(() => import("./views/commands/AllCommands"));
+// const CommandCategory = lazy(() => import("./views/commands/CommandCategory"));
+// const AllLinks = lazy(() => import("./views/links/AllLinks"));
+// const LinkCategory = lazy(() => import("./views/links/LinkCategory"));
+// const AllSnippetsPage = lazy(() => import("./views/snippets/AllSnippets"));
+// const SnippetCategoryPage = lazy(() => import("./views/snippets/SnippetCategory"));
+// const AddSnippetPage = lazy(() => import("./views/snippets/AddSnippetPage"));
 
 const SpinnerFallback: FC = () => (
   <UserLayout>
